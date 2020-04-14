53eb181067d618914dd2eea752f5dd20d77d85f7
bpapp <balazs-tamas.papp@capgemini.com>
2020-04-14 11:03:59 +0200

    Adding unit test

--- src/main/java/training/EmployeeDao.java
+++ src/main/java/training/EmployeeDao.java
@@ -1,8 +1,20 @@
 package training;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class EmployeeDao {
 
+    private List<String> employees = Collections.synchronizedList(new ArrayList<>()); //szinkronizált wrapper
+
     public void saveEmployee(String name) {
+
         System.out.println("Save employee");
+        employees.add(name);
+    }
+
+    public List<String> getEmployees() {
+        return new ArrayList<>(employees);  //Collections.unmodifiableList másik lehetöség
     }
 }
